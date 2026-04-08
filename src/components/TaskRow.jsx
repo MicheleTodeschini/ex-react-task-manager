7f42e7b8ec333c02c5e9ebba8a15f1e484c998db
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-04-08 14:42:50 +0200

    solved point 4 milestone 3

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -3,6 +3,14 @@ import { useGlobalContext } from "../context/GlobalContext";
 export default function TaskRow() {
 
     const { tasks } = useGlobalContext()
+    console.log(tasks);
+
+
+    const statusToClass = {
+        'To do': 'bg-danger',
+        'Doing': 'bg-warning',
+        'Done': 'bg-success'
+    }
 
 
 
@@ -10,11 +18,11 @@ export default function TaskRow() {
 
         <>
             {tasks?.map((task) => (
-                <div className="row" key={task.id}>
-                    <div className="col-sm">
+                <div className='row' key={task.id}>
+                    <div className="col-sm " >
                         <h3 className="bold">{task.title}</h3>
                     </div>
-                    <div className="col-sm ">
+                    <div className={`col-sm ${statusToClass[task.status]}`}>
                         <h3 className="bold">{task.status}</h3>
                     </div>
                     <div className="col-sm">
